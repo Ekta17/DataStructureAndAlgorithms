5bb7141cff5b39869b61469893b2393dcd9af023
Ekta <ekta.arora464@gmail.com>
2021-02-21 22:31:30 -0500

    all permutations of a string

--- src/test/java/string/AllPermutationsOfStringTest.java
+++ src/test/java/string/AllPermutationsOfStringTest.java
@@ -13,10 +13,21 @@ class AllPermutationsOfStringTest {
 	AllPermutationsOfString allPermutationsOfString = new AllPermutationsOfString();
 
 	@Test
-	void getAllPermutations() {
+	void getAllPermutationsWithUniqueChar() {
 		String s = "abc";
 		Set<String> actualPermutations = allPermutationsOfString.getAllPermutations(s);
 		Set<String> expectedPermutations = new HashSet<>(Arrays.asList("abc", "acb", "bca", "bac", "cab", "cba"));
+		assertEquals(expectedPermutations.size(), actualPermutations.size());
+		assertTrue(expectedPermutations.containsAll(actualPermutations));
+		assertTrue(actualPermutations.containsAll(expectedPermutations));
+	}
+
+	@Test
+	void getAllPermutationsWithRepeatingChar() {
+		String s = "aab";
+		Set<String> actualPermutations = allPermutationsOfString.getAllPermutations(s);
+		Set<String> expectedPermutations = new HashSet<>(Arrays.asList("aab", "aba", "baa"));
+		System.out.println(actualPermutations);
 		assertEquals(expectedPermutations.size(), actualPermutations.size());
 		assertTrue(expectedPermutations.containsAll(actualPermutations));
 		assertTrue(actualPermutations.containsAll(expectedPermutations));
