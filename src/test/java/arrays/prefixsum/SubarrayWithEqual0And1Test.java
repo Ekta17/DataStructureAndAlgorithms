aa586341109d96a621853f2eba321e4b41ee5c7f
Ekta <ekta.arora464@gmail.com>
2021-02-27 23:49:17 -0500

    test

--- src/test/java/arrays/prefixsum/SubarrayWithEqual0And1Test.java
+++ src/test/java/arrays/prefixsum/SubarrayWithEqual0And1Test.java
@@ -14,20 +14,17 @@ class SubarrayWithEqual0And1Test {
 		assertEquals(2, subarrayWithEqual0And1.getLength(arr));
 	}
 
-
 	@Test
 	void testGetLength_2() {
 		int arr[] = {1, 1, 0};
 		assertEquals(2, subarrayWithEqual0And1.getLength(arr));
 	}
 
-
 	@Test
 	void testGetLength_3() {
 		int arr[] = {1, 1, 0, 0, 1, 1};
 		assertEquals(4, subarrayWithEqual0And1.getLength(arr));
 	}
-
 
 	@Test
 	void testGetLength_4() {
