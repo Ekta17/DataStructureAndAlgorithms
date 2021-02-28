1f41b558ab12c62210140f698f38a6a25913effe
Ekta <ekta.arora464@gmail.com>
2021-02-28 15:44:40 -0500

    formatting

--- src/test/java/hashing/FirstRepeatingElementTest.java
+++ src/test/java/hashing/FirstRepeatingElementTest.java
@@ -1,21 +1,21 @@
 package hashing;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 class FirstRepeatingElementTest {
   FirstRepeatingElement firstRepeatingElement = new FirstRepeatingElement();
 
   @Test
   void testFirstRepeatedWhenRepeating() {
-    int[] arr = {1,5,3,4,3,5,6};
+    int[] arr = {1, 5, 3, 4, 3, 5, 6};
     assertEquals(2, firstRepeatingElement.firstRepeated(arr));
   }
 
   @Test
   void testFirstRepeatedWhenNotRepeating() {
-    int[] arr = {1,2,3,4};
+    int[] arr = {1, 2, 3, 4};
     assertEquals(-1, firstRepeatingElement.firstRepeated(arr));
   }
 }
